d818ae6a7cb3d60f2969d9175b9344b4e2d2ecdd
Owen Mowery <owenmowery99@gmail.com>
2021-04-02 22:14:10 -0400

    commit tree 466576b5f8b98f918396c09c9ddd1bb2dec0e76b

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Owen Mowery"
+    end
+
+    get '/hometown' do
+        "My hometown is Erie, Pennsylvania"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Notorious B.I.G."
+    end
 end
